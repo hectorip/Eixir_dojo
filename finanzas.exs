33d46ea79c4a70c9320db8f7c2394b7427955355
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2015-10-31 23:56:20 -0600

    Removing not used arguments

--- finanzas.exs
+++ finanzas.exs
@@ -1,6 +1,6 @@
 defmodule Finanzas do
 
-  defp calcaular_iva(precio_con_iva, tasa \\ 1.16, _descuento \\ 10) do
+  defp calcaular_iva(precio_con_iva, tasa \\ 1.16) do
     precio_sin_iva = precio_con_iva / tasa
     { (precio_con_iva - precio_sin_iva), precio_sin_iva }
   end
@@ -9,5 +9,5 @@ defmodule Finanzas do
     {iva, precio_sin_iva} = calcaular_iva(precio_total)
     # precio_sin_iva, iva, precio_total
     :io.format("El desglose del precio es ~nSubotal: ~f;~nIVA: ~f;~nTotal: ~f", [precio_sin_iva, iva, precio_total*1.0])  
-  end 
+  end
 end
